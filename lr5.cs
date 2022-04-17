cee7ed110b38f0e939f68844053a01fcde5e5520
JKiblik <102478047+JKiblik@users.noreply.github.com>
2022-04-17 21:47:06 +0300

    Add files via upload

---
+++ lr5.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Button btn = new Button();
+                btn.Text = "btn" + i;
+                btn.Location = new Point(i * 25, i * 25);
+                label2.Controls.Add(btn);
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                TextBox txtBox = new TextBox();
+                txtBox.Text = "txtBox" + i;
+                txtBox.Location = new Point(i * 25, i * 25);
+                label3.Controls.Add(txtBox);
+            }
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Label lbl = new Label();
+                lbl.Text = "lbl" + i;
+                lbl.Location = new Point(i * 25, i * 25);
+                label1.Controls.Add(lbl);
+            }
+        }
+    }
+}
